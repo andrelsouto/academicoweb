b59db3aff8771ac37c4e5eef28cc427e5f28c1b6
André Luiz <a.luiz.souto@gmail.com>
2017-06-19 08:12:18 -0300

    Add professor business e Repository

---
+++ src/main/java/br/com/switchxiv/academicoweb/dao/ProfessorBusiness.java
@@ -0,0 +1,7 @@
+package br.com.switchxiv.academicoweb.dao;
+
+import br.com.switchxiv.academicoweb.model.Professor;
+
+public class ProfessorBusiness extends GenericBusiness<Professor> implements ProfessorRepository {
+
+}
---
+++ src/main/java/br/com/switchxiv/academicoweb/dao/ProfessorRepository.java
@@ -0,0 +1,7 @@
+package br.com.switchxiv.academicoweb.dao;
+
+import br.com.switchxiv.academicoweb.model.Professor;
+
+public interface ProfessorRepository extends GenericRepository<Professor> {
+
+}
