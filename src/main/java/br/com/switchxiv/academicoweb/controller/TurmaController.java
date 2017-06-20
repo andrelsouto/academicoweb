ddb9ee05c96e657554d0f50f7d0885a490db9679
André Luiz <a.luiz.souto@gmail.com>
2017-06-20 06:05:22 -0300

    add annotations no turma business e criacao da class turma controller

---
+++ src/main/java/br/com/switchxiv/academicoweb/controller/TurmaController.java
@@ -0,0 +1,5 @@
+package br.com.switchxiv.academicoweb.controller;
+
+public class TurmaController {
+
+}
--- src/main/java/br/com/switchxiv/academicoweb/dao/TurmaBusiness.java
+++ src/main/java/br/com/switchxiv/academicoweb/dao/TurmaBusiness.java
@@ -1,7 +1,12 @@
 package br.com.switchxiv.academicoweb.dao;
 
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 import br.com.switchxiv.academicoweb.model.Turma;
 
+@Repository
+@Transactional
 public class TurmaBusiness extends GenericBusiness<Turma> implements TurmaRepository {
 
 }
