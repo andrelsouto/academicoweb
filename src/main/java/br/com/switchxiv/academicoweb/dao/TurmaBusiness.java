0b9aaf33d6fdee66c18c1af31ca4e88196738638
André Luiz <a.luiz.souto@gmail.com>
2017-06-20 06:04:12 -0300

    dao de turma

---
+++ src/main/java/br/com/switchxiv/academicoweb/dao/TurmaBusiness.java
@@ -0,0 +1,7 @@
+package br.com.switchxiv.academicoweb.dao;
+
+import br.com.switchxiv.academicoweb.model.Turma;
+
+public class TurmaBusiness extends GenericBusiness<Turma> implements TurmaRepository {
+
+}
---
+++ src/main/java/br/com/switchxiv/academicoweb/dao/TurmaRepository.java
@@ -0,0 +1,7 @@
+package br.com.switchxiv.academicoweb.dao;
+
+import br.com.switchxiv.academicoweb.model.Turma;
+
+public interface TurmaRepository extends GenericRepository<Turma> {
+
+}
